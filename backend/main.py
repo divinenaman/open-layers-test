fd163fb21365a4a3f2dcdfaf3668c8b0529beb69
Naman Agarwal <namanddev7@gmail.com>
2022-10-20 03:25:08 +0530

    status codes, cookie

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,8 @@
 from typing import Union
 from unicodedata import name
 from pydantic import BaseModel
-from fastapi import FastAPI, Response, Cookie
+from fastapi import FastAPI, Response, Cookie, HTTPException
+from fastapi.responses import JSONResponse
 from datetime import datetime, timezone, timedelta
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -29,13 +30,16 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["GET", "POST", "HEAD", "OPTIONS"],
+    allow_headers=["Access-Control-Allow-Headers", 'Content-Type', 'Authorization', 'Access-Control-Allow-Origin'],
 )
 
 @app.post("/login")
-async def login(login: Login, response: Response):
+async def login(login: Login):
     res = await userRepo.getUserUsingEmail(login.email, login.password)
+
+    if res["error"] == True:
+        raise HTTPException(status_code=400, detail="no user found")
 
     token = jwt.encode({
         "exp": datetime.now(tz=timezone.utc) + timedelta(minutes=30),
@@ -44,25 +48,29 @@ async def login(login: Login, response: Response):
     }, secret)
 
     if token["error"] == True:
-        return {"status": False}
+        raise HTTPException(status_code=400, detail="token not generated")
 
-    response.set_cookie(key="t", value=token["payload"])
+    response = JSONResponse(content={
+        "name": res["name"],
+        "email": res["email"]
+    })
 
-    return res
+    response.set_cookie(key="t", value=token["payload"], httponly=True, secure=True)
+
+    return response
 
 
 @ app.get("/verify")
 def verify(t: Union[str, None]=Cookie(default=None)):
     if t == None:
-        return {"status": False}
+        raise HTTPException(status_code=400, detail="cannot authenticate")
 
     d = jwt.decode(t, secret)
 
-    if d["error"] == False:
-        return {"status": True}
+    if d["error"] == True:
+        raise HTTPException(status_code=400, detail="cannot authenticate")
 
-    else:
-        return {"status": False}
+    return {"status": True, "name": d["payload"]["name"], "email": d["payload"]["email"] }
 
 
 @ app.post("/signUp")
--- backend/repo/user.py
+++ backend/repo/user.py
@@ -5,7 +5,7 @@ async def getUserUsingEmail(email, password):
 
     # get user from db
 
-    return { "name": "naman", "email": "namanddev7@gmail.com" }
+    return { "error": False, "name": "naman", "email": "namanddev7@gmail.com" }
 
 async def addUser(email, password, name):
 
